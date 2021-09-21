e282e7b87ec2fa993424f7cdd1826d348276a2fd
raghuk134 <k.r.rao@outlook.com>
2021-09-21 08:57:52 +0530

    Update HelloController.java

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Spring Boot!";
+        return "updated Hello from Spring Boot!";
     }   
 }
